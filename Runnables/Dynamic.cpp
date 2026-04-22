9c28b218da5efe69e30bfe02d8f89e7007cc9312
Linus Reuter <develop@linusreuter.eu>
2026-04-22 11:31:45 +0200

    Basis for Dynamic Executable

---
+++ Runnables/Dynamic.cpp
@@ -0,0 +1,20 @@
+#include "Commands/Dynamic.h"
+#include "../Helpers/Console/CommandLineParser.h"
+#include "../Helpers/MultiThreading.h"
+#include "../Shell/Shell.h"
+
+using namespace Shell;
+
+int main(int argc, char** argv) {
+
+    CommandLineParser clp(argc, argv);
+    pinThreadToCoreId(clp.value<int>("core", 1));
+    checkAsserts();
+    ::Shell::Shell shell;
+
+    new RAPTORToDynamic(shell);
+    new LoadAndApplyDynamicPartition(shell);
+
+    shell.run();
+    return 0;
+}
